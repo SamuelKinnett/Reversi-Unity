b64208e5da53a42e2ce4915767c1c361956f4302
Samuel Kinnett <samuelkinnett@member.fsf.org>
2015-04-17 21:37:51 +0100

    Initial Commit

---
+++ Assets/Scripts/BoardBehaviour.cs
@@ -0,0 +1,254 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardBehaviour : MonoBehaviour {
+
+	public int currentPlayer;	//1 = player 1, 2 = player 2
+	int [,] board;				//The gameboard. A 0 indicates an uncaptured tile, whilst a 1 or a 2 indicates ownership by player 1 or 2 respectively.
+
+	// Use this for initialization
+	void Start () {
+
+		board = new int[8, 8];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Get the owner of the tile at the passed co-ordinates
+	public int GetTileState(int x, int y) {
+
+		return board [x, y];
+
+	}
+
+	//Set the state of the tile at the passed co-ordinates and update all other tiles accordingly
+	public void SetTileState(int x, int y, int owner) {
+
+		board [x, y] = owner;
+
+		#region Test Right
+		int length = 0;
+		bool endFound = false;
+
+		for (int currentX = x + 1; currentX < 8; currentX ++) {
+
+			if (board[currentX, y] == owner || endFound == true) {
+				endFound = true;
+			}
+			else
+			{
+				length++;
+			}
+		}
+		if (endFound) {
+			length++;
+			for (int currentX = x + 1; currentX < x + length; currentX ++) {
+				board[currentX, y] = owner;
+			}
+		}
+		#endregion
+
+		#region Test Left
+		length = 0;
+		endFound = false;
+
+		for (int currentX = x - 1; currentX >= 0; currentX --) {
+			
+			if (board[currentX, y] == owner || endFound == true) {
+				endFound = true;
+			}
+			else
+			{
+				length++;
+			}
+		}
+		if (endFound) {
+			length++;
+			for (int currentX = x - 1; currentX > x - length; currentX --) {
+				board[currentX, y] = owner;
+			}
+		}
+		#endregion
+
+		#region Test Upwards
+		length = 0;
+		endFound = false;
+
+
+		for (int currentY = y - 1; currentY >= 0; currentY --) {
+			
+			if (board[x, currentY] == owner || endFound == true) {
+				endFound = true;
+			}
+			else
+			{
+				length++;
+			}
+		}
+		if (endFound) {
+			length++;
+			for (int currentY = y - 1; currentY >= y - length; currentY --) {
+				board[x, currentY] = owner;
+			}
+		}
+		#endregion
+
+		#region Test Downwards
+		length = 0;
+		endFound = false;
+		
+		
+		for (int currentY = y + 1; currentY < 8; currentY ++) {
+			
+			if (board[x, currentY] == owner || endFound == true) {
+				endFound = true;
+			}
+			else
+			{
+				length++;
+			}
+		}
+		if (endFound) {
+			length++;
+			for (int currentY = y + 1; currentY < y + length; currentY ++) {
+				board[x, currentY] = owner;
+			}
+		}
+		#endregion
+
+		#region Test Up-Right
+		length = 0;
+		endFound = false;
+
+		int diagY = y;
+
+		for (int currentX = x + 1; currentX < 8; currentX ++) {
+			if (diagY > 0) {
+				diagY--;
+
+			if (board[currentX, diagY] == owner || endFound == true)
+				endFound = true;
+			else
+				length++;
+			}
+		}
+
+		diagY = y;
+
+		if (endFound) {
+			length++;
+			for (int currentX = x + 1; currentX < x + length; currentX ++) {
+				if (diagY > 0)
+				{
+					diagY--;
+					board[currentX, diagY] = owner;
+				}
+			}
+		}
+		#endregion
+
+		#region Up-Left
+		length = 0;
+		endFound = false;
+		
+		diagY = y;
+		
+		for (int currentX = x - 1; currentX >= 0; currentX --) {
+			if (diagY > 0) {
+				diagY--;
+				
+			if (board[currentX, diagY] == owner || endFound == true)
+				endFound = true;
+			else
+				length++;
+			}
+		}
+		
+		diagY = y;
+		
+		if (endFound) {
+			length++;
+			for (int currentX = x - 1; currentX > x - length; currentX --) {
+				if (diagY > 0)
+				{
+					diagY--;
+					board[currentX, diagY] = owner;
+				}
+			}
+		}
+		#endregion
+
+		#region Down-Right
+		length = 0;
+		endFound = false;
+		
+		diagY = y;
+		
+		for (int currentX = x + 1; currentX < 8; currentX ++) {
+			if (diagY < 7) {
+				diagY++;
+				
+				if (board[currentX, diagY] == owner || endFound == true)
+					endFound = true;
+				else
+					length++;
+			}
+		}
+		
+		diagY = y;
+		
+		if (endFound) {
+			length++;
+			for (int currentX = x + 1; currentX < x + length; currentX ++) {
+				if (diagY < 7)
+				{
+					diagY++;
+					board[currentX, diagY] = owner;
+				}
+			}
+		}
+		#endregion
+
+		#region Down-Left
+		length = 0;
+		endFound = false;
+		
+		diagY = y;
+		
+		for (int currentX = x - 1; currentX >= 0; currentX --) {
+			if (diagY < 7) {
+				diagY++;
+				
+				if (board[currentX, diagY] == owner || endFound == true)
+					endFound = true;
+				else
+					length++;
+			}
+		}
+		
+		diagY = y;
+		
+		if (endFound) {
+			length++;
+			for (int currentX = x - 1; currentX >= x - length; currentX --) {
+				if (diagY < 7)
+				{
+					diagY++;
+					board[currentX, diagY] = owner;
+				}
+			}
+		}
+		#endregion
+
+	}
+
+	//Returns an integer representing the number of tiles that will be converted should a tile be placed at the passed co-ordinates
+	public int GetTileScore(int x, int y, int owner) {
+
+		return 0;
+	}
+}
---
+++ Assets/Scripts/TileBehaviour.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileBehaviour : MonoBehaviour {
+
+	public GameObject boardManager;			//The boardBehaviour script to allow access to the current board state
+	public SpriteRenderer spriteRenderer;	//The Sprite Renderer for this tile
+	public int x;							//The X co-ordinate of the tile
+	public int y;							//The Y co-ordinate of the tile
+	public float fadeSpeed;
+
+	Color tintColour;						//The colour to tint the square with
+	int owner;								//1 = player 1, 2 = player 2, 0 = unowned
+	float opacity;							//The opacity of the tile
+	private BoardBehaviour boardBehaviour;	//The BoardBehaviour script of the BoardManager 
+
+	bool fading;							//Is the tile currently fading to another colour?
+	Color currentColour;					//The current colour
+	float currentOpacity;					//The current opacity
+	float time;								//Used to store a time value for the purpose of fading
+
+	// Use this for initialization
+	void Start () {
+
+		tintColour = Color.white;
+		opacity = 0.5f;
+		boardBehaviour = boardManager.GetComponent<BoardBehaviour> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		spriteRenderer.material.SetColor ("_Color", currentColour);
+
+		owner = boardBehaviour.GetTileState (x, y);	//Who does this tile belong to?
+
+		switch (owner) {
+
+		case 0:
+			tintColour = Color.white;
+			break;
+
+		case 1:
+			tintColour = Color.red;
+			break;
+
+		case 2:
+			tintColour = Color.blue;
+			break;
+		}
+
+		if (currentColour != tintColour || currentOpacity != opacity) {
+
+			time = 0;
+			fading = true;
+
+		} else {
+
+			fading = false;
+
+		}
+
+		if (fading) {
+
+			time += Time.deltaTime;
+			currentColour = Color.Lerp(currentColour, tintColour, time * fadeSpeed);
+			currentOpacity = Mathf.Lerp (currentOpacity, opacity, time * fadeSpeed);
+
+			currentColour.a = currentOpacity;
+		}
+	}
+
+	// When the mouse is passed over the tile
+	void OnMouseOver() {
+
+		opacity = 1;
+
+	}
+
+	//When the mouse leaves the tile
+	void OnMouseExit() {
+
+		opacity = 0.5f;
+
+	}
+
+	//When the tile is clicked
+	void OnMouseDown() {
+
+		if (boardBehaviour.currentPlayer == 1) {
+			boardBehaviour.SetTileState(x, y, 1);
+		}
+	}
+}
